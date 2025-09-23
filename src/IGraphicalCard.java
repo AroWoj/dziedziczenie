95d4dcaacb933030c7c19b65fb128ebb04eddc3a
voytasic <arekw37@o2.pl>
2025-09-23 08:13:10 +0200

    Add graphics processor

--- src/IGraphicalCard.java
+++ src/IGraphicalCard.java
@@ -1,3 +1,4 @@
 public interface IGraphicalCard {
     String getGraphicsCard();
+    String getGraphicsProcessor();
 }
--- src/PersonalComputer.java
+++ src/PersonalComputer.java
@@ -14,4 +14,9 @@ public class PersonalComputer extends MainBoard implements IComputer, IGraphical
     public String getGraphicsCard() {
         return "NVidia 1GB RAM Pro";
     }
+
+    @Override
+    public String getGraphicsProcessor() {
+        return "NVidia i7 Compact 512Bit";
+    }
 }
